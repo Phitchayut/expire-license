b37ba9b9322cd4b2cfc6e0a0cd6ec043cfe22609
Phitchayut <70578824+Phitchayut@users.noreply.github.com>
2023-08-21 17:22:05 +0700

    Update View.jsx

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -16,30 +16,32 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 const View = () => {
   return (
-    <div className="mx-auto container mt-40">
+    <>
       <Navbar />
-      <Card className="font-poppins">
-        <CardHeader
-          variant="gradient"
-          color="blue"
-          className="mb-8 p-6 flex justify-between items-center"
-        >
-          <Typography variant="h5" color="white">
-            View Detail
-          </Typography>
-          <Link to="/license">
-            <Button
-              color="orange"
-              variant="gradient"
-              className="flex items-center justify-center w-[8rem]"
-            >
-              <BsBackspace className="text-xl mr-1" />
-              Back
-            </Button>
-          </Link>
-        </CardHeader>
-      </Card>
-    </div>
+      <div className="mx-auto container mt-14">
+        <Card className="font-poppins">
+          <CardHeader
+            variant="gradient"
+            color="blue"
+            className="mb-8 p-6 flex justify-between items-center"
+          >
+            <Typography variant="h5" color="white">
+              View Detail
+            </Typography>
+            <Link to="/license">
+              <Button
+                color="orange"
+                variant="gradient"
+                className="flex items-center justify-center w-[8rem]"
+              >
+                <BsBackspace className="text-xl mr-1" />
+                Back
+              </Button>
+            </Link>
+          </CardHeader>
+        </Card>
+      </div>
+    </>
   );
 };
 
