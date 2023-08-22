8bcb87791b079d235429dcc439cb7d151f734416
phitchayut <70578824+Phitchayut@users.noreply.github.com>
2023-08-22 07:17:29 +0000

    Update 6 files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,9 +39,9 @@ export default function StickyNavbar() {
   );
 
   return (
-    <div className="">
-      <Navbar className="fixed top-6 z-10 h-max w-full rounded-xl py-2 px-4 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900">
+    <div className="px-3 lg:px-0">
+      <Navbar className="mx-auto w-full py-2 px-4 lg:px-8 lg:py-4 mt-3">
+        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
           <Link to="/">
             <div className="flex items-center h-[3rem]">
               <img
@@ -61,48 +61,48 @@ export default function StickyNavbar() {
               </Typography>
             </div>
           </Link>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
-          </div>
+          <div className="hidden lg:block">{navList}</div>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
         </div>
-        <MobileNav open={openNav}>{navList}</MobileNav>
+        <MobileNav open={openNav}>
+          <div className="container mx-auto">{navList}</div>
+        </MobileNav>
       </Navbar>
     </div>
   );
--- src/pages/License.jsx
+++ src/pages/License.jsx
@@ -57,115 +57,117 @@ const TABLE_ROWS = [
 
 const License = () => {
   return (
-    <div className="mx-auto container mt-40">
+    <>
       <Navbar />
-      <Card className="font-poppins">
-        <CardHeader
-          variant="gradient"
-          color="blue"
-          className="mb-8 p-6 flex justify-between items-center"
-        >
-          <Typography variant="h5" color="white">
-            Expire License
-          </Typography>
-          <Link to="/create">
-            <Button
-              color="green"
-              variant="gradient"
-              className="flex items-center justify-center w-[8rem]"
-            >
-              <AiOutlinePlusCircle className="text-xl mr-1" /> Add
-            </Button>
-          </Link>
-        </CardHeader>
-        <CardBody className="overflow-x-scroll px-3 pt-0 pb-2">
-          <table className="w-full min-w-max table-auto text-left">
-            <thead>
-              <tr>
-                {TABLE_HEAD.map((head) => (
-                  <th
-                    key={head}
-                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                  >
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70 text-center"
+      <div className="mx-auto container mt-14">
+        <Card className="font-poppins">
+          <CardHeader
+            variant="gradient"
+            color="blue"
+            className="mb-8 p-6 flex justify-between items-center"
+          >
+            <Typography variant="h5" color="white">
+              Expire License
+            </Typography>
+            <Link to="/create">
+              <Button
+                color="green"
+                variant="gradient"
+                className="flex items-center justify-center w-[8rem]"
+              >
+                <AiOutlinePlusCircle className="text-xl mr-1" /> Add
+              </Button>
+            </Link>
+          </CardHeader>
+          <CardBody className="overflow-x-scroll px-3 pt-0 pb-2">
+            <table className="w-full min-w-max table-auto text-left">
+              <thead>
+                <tr>
+                  {TABLE_HEAD.map((head) => (
+                    <th
+                      key={head}
+                      className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                     >
-                      {head}
-                    </Typography>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {TABLE_ROWS.map(({ name, job, date }, index) => {
-                const isLast = index === TABLE_ROWS.length - 1;
-                const classes = isLast
-                  ? "p-4"
-                  : "p-4 border-b border-blue-gray-50";
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal leading-none opacity-70 text-center"
+                      >
+                        {head}
+                      </Typography>
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {TABLE_ROWS.map(({ name, job, date }, index) => {
+                  const isLast = index === TABLE_ROWS.length - 1;
+                  const classes = isLast
+                    ? "p-4"
+                    : "p-4 border-b border-blue-gray-50";
 
-                return (
-                  <tr key={name} className=" text-center">
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {name}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {job}
-                      </Typography>
-                    </td>
-                    <td className={classes}>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="font-normal"
-                      >
-                        {date}
-                      </Typography>
-                    </td>
-                    <td className="">
-                      <Typography
-                        as="a"
-                        href="#"
-                        className="font-medium text-white space-x-2"
-                      >
-                        <Link to="/view">
-                          <IconButton color="orange">
-                            <AiOutlineFolderView />
-                          </IconButton>
-                        </Link>
-                        <Link to="/update">
-                          <IconButton color="blue">
-                            <AiOutlineEdit />
-                          </IconButton>
-                        </Link>
-                        <IconButton
-                          color="red"
-                          onClick={() => confirm("Delete ?")}
+                  return (
+                    <tr key={name} className=" text-center">
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
                         >
-                          <AiOutlineDelete />
-                        </IconButton>
-                      </Typography>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </CardBody>
-      </Card>
-    </div>
+                          {name}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {job}
+                        </Typography>
+                      </td>
+                      <td className={classes}>
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {date}
+                        </Typography>
+                      </td>
+                      <td className="">
+                        <Typography
+                          as="a"
+                          href="#"
+                          className="font-medium text-white space-x-2"
+                        >
+                          <Link to="/view">
+                            <IconButton color="orange">
+                              <AiOutlineFolderView />
+                            </IconButton>
+                          </Link>
+                          <Link to="/update">
+                            <IconButton color="blue">
+                              <AiOutlineEdit />
+                            </IconButton>
+                          </Link>
+                          <IconButton
+                            color="red"
+                            onClick={() => confirm("Delete ?")}
+                          >
+                            <AiOutlineDelete />
+                          </IconButton>
+                        </Typography>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </CardBody>
+        </Card>
+      </div>
+    </>
   );
 };
 
